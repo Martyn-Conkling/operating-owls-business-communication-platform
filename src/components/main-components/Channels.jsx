62fc3eb43b82a7131cb3da24e6369c67a0c25890
AshSensei <an9simbol@gmail.com>
2024-02-13 20:03:57 -0800

    channels more reusable with components & js data

--- src/components/main-components/Channels.jsx
+++ src/components/main-components/Channels.jsx
@@ -7,34 +7,39 @@ import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Box from '@mui/material/Box';
-import TagIcon from '@mui/icons-material/Tag';
-
+import IndivChannel from '../IndivChannel';
 import AddIcon from '@mui/icons-material/Add';
-
-
-
-    
-
+import channelData from '../../data/channelData'
 
 export default function Channels(){
     const [open, setOpen] = React.useState(true); //for the collapsible list
+    const [channels, setChannels] = React.useState(channelData);
+
     const [selectedIndex, setSelectedIndex] = React.useState(1); //for the selected list item
+
     const handleClick = () => {
         setOpen(!open);
     };
+
+
     const handleListItemClick = (event, index) => {
         setSelectedIndex(index);
-      };
+    };
     
+    const channelElements = channels.map(channel => (
+        <IndivChannel 
+            key={channel.id}
+            name={channel.name}
+        />
+    ))
+
     return (
         <>
             <h1>Welcome to Ashton's Test page</h1>
             <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}> 
                 <nav>
                     <List component="nav">
-                         
 
-                        
                         <ListItemButton onClick={handleClick} sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
                             <ListItemButton sx={{ width:'5%',  maxWidth: 10, display:'flex', justifyContent: 'center' }} onClick={(e) => { e.stopPropagation(); }}>
                                 <ListItemIcon sx={{display:'flex', justifyContent: 'center' }}>
@@ -52,54 +57,10 @@ export default function Channels(){
                             
                             
                         </ListItemButton>
-                        
-                        
-                      
+    
                         <Collapse in={open} unmountOnExit>
                             <List component="div">
-                                <ListItemButton 
-                                    sx={{ pl: 4 }} 
-                                    selected={selectedIndex === 1}
-                                    onClick={(event) => handleListItemClick(event, 1)} 
-                                >
-                                    <ListItemIcon>
-                                        <TagIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="announcements" />
-                                </ListItemButton>
-
-                                <ListItemButton 
-                                    sx={{ pl: 4 }} 
-                                    selected={selectedIndex === 2}
-                                    onClick={(event) => handleListItemClick(event, 2)} 
-                                >
-                                    <ListItemIcon>
-                                        <TagIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="general" />
-                                </ListItemButton>
-                                
-                                <ListItemButton 
-                                    sx={{ pl: 4 }} 
-                                    selected={selectedIndex === 3}
-                                    onClick={(event) => handleListItemClick(event, 3)} 
-                                >
-                                    <ListItemIcon>
-                                        <TagIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="serious-stuff" />
-                                </ListItemButton>
-
-                                <ListItemButton 
-                                    sx={{ pl: 4 }} 
-                                    selected={selectedIndex === 4}
-                                    onClick={(event) => handleListItemClick(event, 4)} 
-                                >
-                                    <ListItemIcon>
-                                        <TagIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="memes" />
-                                </ListItemButton>
+                                {channelElements}
                             </List>
                         </Collapse>
                        
