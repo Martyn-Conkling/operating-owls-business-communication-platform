fcbc9b65d1ac1770e30ee044941336c075215020
elizabethv1998 <142066779+elizabethv1998@users.noreply.github.com>
2024-02-15 18:31:21 -0800

    basic badge comp

---
+++ src/components/main-components/Badges.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Badges = () => {
+    return(
+    <div>
+        <h1> Welcome to Our Community</h1>
+    <div>
+    <h2>Pinned Channels</h2>  
+    {/* the badges for pinned channels here*/} 
+    <span>Channel 1</span> 
+    <span>Channel 2</span>
+    <span>Channel 3</span>
+    </div>
+    <div>
+    <h2>Active Channels</h2>
+    {/*same here, add badges*/}
+    <span>Channel 1</span>
+    </div>
+    </div>    
+    ); };
+    export default Badges;
