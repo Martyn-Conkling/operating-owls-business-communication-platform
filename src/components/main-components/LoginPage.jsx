967712df7cbe75f72dbeae5c45db5e41c49a58ac
xowin <Christianrodrigues02@gmail.com>
2024-02-21 14:03:40 -0800

    fixed login page

--- src/components/main-components/LoginPage.jsx
+++ src/components/main-components/LoginPage.jsx
@@ -8,7 +8,7 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import BusinessIcon from './BusinessIcon';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -28,8 +28,8 @@ export default function ChristianTestPage() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
+          <Avatar sx={{ m: 1, bgcolor: 'black' }}>
+            <BusinessIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
