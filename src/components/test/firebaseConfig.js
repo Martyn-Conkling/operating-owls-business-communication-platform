8f7474bbda09417ea6fdc54f350df4273136a0a2
Miles Angels <milesmarieangels@gmail.com>
2024-02-24 09:06:08 -0800

    removed pages folder

--- src/components/test/firebaseConfig.js
+++ src/components/test/firebaseConfig.js
@@ -9,6 +9,7 @@ import { getAuth } from "firebase/auth"; //for authentication
 import { getFirestore} from "firebase/firestore" //for database storage
 
 // ENV are stored in .env.local file
+// All this information can be found in firebase
 const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const authDom = import.meta.env.VITE_AUTH_DOM;
 const projId = import.meta.env.VITE_PROJ_ID;
