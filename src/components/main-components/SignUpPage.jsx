2a89d063f899ce941ee78a46d8dee914411d20d3
CarlBalansag <carl@techccb.com>
2024-02-25 18:06:08 -0800

    updating sign up page

--- src/components/main-components/SignUpPage.jsx
+++ src/components/main-components/SignUpPage.jsx
@@ -11,8 +11,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from 'react';
 import { auth } from '../test/firebaseConfig'; 
 import { Grid } from '@mui/material';
+import LightBulbImage from './LightBulb';
 import BusinessIcon from './BusinessIcon';
-
 
 export default function SignUpPage() {
 
@@ -42,27 +42,31 @@ export default function SignUpPage() {
             alignItems: 'center',
             }}
         >
-            <Grid container spacing={0}>
+            <Grid container spacing={-1}>
                 <Grid item xs={6}>
                     <Avatar sx={{ m: 1, bgcolor: 'black', ml: 33, mb: 3}}>
                         <BusinessIcon />
                     </Avatar>
-                    <Typography variant="h5">
+                    <Typography variant="h5" sx={{ml:30}}>
                     Welcome
                     </Typography>
                     <Typography variant="h7" sx={{
                         textDecoration: 'underline',
-                        paddingTop: '3px'
+                        paddingTop: '3px',
+                        ml: 28
                     }}>
                     Lets get you set up
                     </Typography>
                     <Box component="form" noValidate autoComplete="off" sx={{ mt: 1 }}>
                         <TextField
+                            sx={{width: 420,
+                                ml:10
+                                
+                            }}
                             onChange={e => setEmail(e.target.value)}
                             value={email}
                             margin="normal"
                             required
-                            fullWidth
                             id="email"
                             label="Email Address"
                             name="email"
@@ -70,11 +74,13 @@ export default function SignUpPage() {
                             autoFocus
                         />
                         <TextField
+                            sx={{width: 420,
+                                ml:10
+                            }}                           
                             onChange={e => setUsername(e.target.value)}
                             value={username}
                             margin="normal"
                             required
-                            fullWidth
                             id="username"
                             label="Username"
                             name="username"
@@ -82,11 +88,13 @@ export default function SignUpPage() {
                             autoFocus
                         />
                         <TextField
+                            sx={{width: 420,
+                                ml:10
+                            }}                        
                             onChange={e => setPassword(e.target.value)}
                             value={password}
                             margin="normal"
                             required
-                            fullWidth
                             name="password"
                             label="Password"
                             type="password"
@@ -94,11 +102,13 @@ export default function SignUpPage() {
                             autoComplete="current-password"
                         />
                         <TextField
+                            sx={{width: 420,
+                                ml:10
+                            }}                        
                             onChange={e => setConfirmPassword(e.target.value)}
                             value={confirmPassword}
                             margin="normal"
                             required
-                            fullWidth
                             name="confirm password"
                             label="Confirm Password"
                             type="password"
@@ -107,21 +117,21 @@ export default function SignUpPage() {
                             // error={!validate}
                             // helperText={error}
                         />
-                        <Button
+                        <Button                   
                             onClick={validate}
                             type="button"
-                            fullWidth
                             color='secondary'
                             variant="contained"
-                            sx={{ mt: 3, mb: 2, borderRadius: 3, }}
+                            sx={{ mt: 3, mb: 2, borderRadius: 3, width: 420, ml:10}}
                         >
                             Sign Up
                         </Button>
-                        <p>Already have an account? <Link color={'secondary'} href={`christian-test-page`}>Login</Link></p>
+                        <br/>
+                        <Typography variant="h7" sx={{ml:23}}>Already have an account? <Link color={'secondary'} href={`christian-test-page`}>Login</Link></Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={6}>
-                    <p>Img</p>
+                    <LightBulbImage/>
                 </Grid>
             </Grid>
         </Box>
