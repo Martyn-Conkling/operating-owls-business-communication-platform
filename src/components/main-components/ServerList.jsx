bb2164cc55df412d816be194614e2066010dbc88
AshSensei <an9simbol@gmail.com>
2024-03-14 20:08:40 -0700

    added basic assign favorite function to servers

--- src/components/main-components/ServerList.jsx
+++ src/components/main-components/ServerList.jsx
@@ -11,15 +11,22 @@ import { Outlet, Link } from 'react-router-dom';
 
 
 export default function ServerList(){
-
-    
+    const [serverList, setServerList] = React.useState(itemData)
+    const setFavorites = (id) => {
+        setServerList( (oldServers) => {
+            const newServers = [...oldServers]
+            newServers[id].favorite = !newServers[id].favorite
+            return newServers
+        })
+        
+    }
 
 
     return(
         <>
             <h1>Server List</h1>
             <ImageList variant="masonry" cols={7} gap={10} sx={{  width: '100%', maxWidth: 1200, height: 1200 }}>
-                {itemData.map((server) => (
+                {serverList.map((server) => (
                 <ImageListItem key={server.img} sx={{width:150, height: 150}} component={Link} to={`/ServerComponent`}>
                     <img src={`${server.img}?w=150&h=150&fit=crop&auto=format`}
                     loading="lazy"/>
@@ -29,8 +36,12 @@ export default function ServerList(){
                     <IconButton
                         sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
                         aria-label={`info about ${server.title}`}
+                        onClick={(event) => {
+                            event.preventDefault(); // Prevent the server page from opening
+                            setFavorites(server.id)
+                        }}
 
-                    ><StarBorderIcon /></IconButton>}/>
+                    >{server.favorite ? <StarIcon/> : <StarBorderIcon/> }</IconButton>}/>
                 </ImageListItem>
                 ))}
             </ImageList>
@@ -44,84 +55,83 @@ const itemData = [
       img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
       id: 0,
       title: 'Breakfast',
-      author: '@bkristastucchio',
       rows: 2,
       cols: 2,
       featured: true,
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
       id: 1,
       title: 'Burger',
-      author: '@rollelflex_graphy726',
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
       id: 2,
       title: 'Camera',
-      author: '@helloimnik',
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
       id: 3,
       title: 'Coffee',
-      author: '@nolanissac',
       cols: 2,
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
       id: 4,
       title: 'Hats',
-      author: '@hjrc33',
       cols: 2,
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
       id: 5,
       title: 'Honey',
-      author: '@arwinneil',
       rows: 2,
       cols: 2,
       featured: true,
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
       id: 6,
       title: 'Basketball',
-      author: '@tjdragotta',
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
       id: 7,
-      title: 'Fern',
-      author: '@katie_wasserman',
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
       id: 8,
       title: 'Mushrooms',
-      author: '@silverdalex',
       rows: 2,
       cols: 2,
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
       id: 9,
       title: 'Tomato basil',
-      author: '@shelleypauls',
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
       id: 10,
       title: 'Sea star',
-      author: '@peterlaster',
+      favorite: false,
     },
     {
       img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
       id: 11,
       title: 'Bike',
-      author: '@southside_customs',
       cols: 2,
+      favorite: false,
     },
     
   ];
