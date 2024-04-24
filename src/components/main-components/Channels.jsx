ecb8482091700d104fd543e9729df7ff35cd3e84
AshSensei <an9simbol@gmail.com>
2024-04-23 18:15:48 -0700

    added confirmation message for deleting channel

--- src/components/main-components/Channels.jsx
+++ src/components/main-components/Channels.jsx
@@ -19,23 +19,46 @@ import TagIcon from '@mui/icons-material/Tag';
 import PropTypes from 'prop-types';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useMyContext } from '../../DataContext';
-
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import IndivChannel from '../IndivChannel';
 //Channel Component displays selection of channels for viewing selection
 
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 export default function Channels(props){
 
     const {serverData, createNewChannel, deleteChannel} = useMyContext();
     const [open, setOpen] = React.useState(true); //for the collapsible list
+    const [openAlert, setOpenAlert] = React.useState(false);
     const channels = serverData; //no longer need react state 
     //const [channels, setChannels] = React.useState(serverData); //to ensure channels update whenever we edit
     const [showModal, setShowModal] = React.useState(false); //for modal reveal and hide
-    const [selectedChannel, setSelectedChannel] = React.useState(props.defaultChannel); //for the selected list item visual
+    const [selectedChannel, setSelectedChannel] = React.useState(serverData.channels.allIds[0]); //for the selected list item visual
 
-
-    //moved from componentized indivChannel to returning it within this component
+    const handleClickOpen = () => {
+        setOpenAlert(true);
+      };
+    
+      const handleClose = () => {
+        setOpenAlert(false);
+      };
+      {/*
+    //using IndivChannel component because it is smoother (unsure why exactly)
+    
     const IndivChannel = (props) => {
+        console.log('Selected:', props.selected);
+        
         return (
+            
             <ListItem 
                 sx={{
                     '&:hover .hover-visible': {
@@ -66,7 +89,7 @@ export default function Channels(props){
             </ListItem>       
             );
     };
-
+    */}
    
     //console.log('Selected Index ' + selectedIndex);
     //handles the collapsible menu
@@ -95,7 +118,6 @@ export default function Channels(props){
     //deletes channel by filtering and selecting the channel to delete
     const deleteItem = (itemId) => {
         const prevChannels = serverData;
-
         //procedure to go to the channel directly above the deleted channel
         if(itemId === selectedChannel){
             const newIndex = prevChannels.channels.allIds.indexOf(itemId) > 0 ? prevChannels.channels.allIds.indexOf(itemId) - 1 : 0;
@@ -126,15 +148,35 @@ export default function Channels(props){
             name={channel?.name}
             onClick={(event)=> handleListItemClick(event, channel?.id)}
             selected={selectedChannel === channel?.id}
-            deleteChannel={()=> deleteItem(channel?.id)}
+            deleteChannel={handleClickOpen}
         />
         )
     })
-    
+
     return (
         <>
             {/* Houses the list of channels */}
-            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}> 
+            <Dialog
+                open={openAlert}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                
+            >
+                <DialogTitle>{"Delete Channel?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete this channel?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button color="error" onClick={handleClose}>Cancel</Button>
+                    <Button color="success" onClick={() => {handleClose(); deleteItem(selectedChannel);}}>Yes</Button>
+                </DialogActions>
+            </Dialog>
+
+
+            <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', height: '100vh', maxWidth: 360, bgcolor: 'background.paper' }}> 
                 <nav>
                     <List component="nav">
                         
@@ -148,15 +190,8 @@ export default function Channels(props){
                         >
                         {/* Channel Header*/}
                             <ListItemButton onClick={handleCollapseClick} sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
-
-                                {/*
-                                <ListItemIcon>
-                                    <InboxIcon />
-                                </ListItemIcon>
-                                */}
                                 <ListItemText primary="Channels" sx={{pl:1, }}  primaryTypographyProps={{ fontWeight: 'bold' }}/>
                                 {open ? <ExpandLess /> : <ExpandMore />}
-
                             </ListItemButton>
                         </ListItem>
                         {/* channel elements are displayed within the div */}
